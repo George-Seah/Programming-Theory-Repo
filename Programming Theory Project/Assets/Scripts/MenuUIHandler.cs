bbf125e1420ec4ac4947594b0de0c697e44b510f
George-Seah <georgerseah@gmail.com>
2023-07-03 19:52:14 +0800

    Functionality added
    Added an exit button and a Highscore Tracker all linked to the canvas. May need to make other adjustments to the menu but seems fine for now. Will work on the base game.

--- Programming Theory Project/Assets/Scripts/MenuUIHandler.cs
+++ Programming Theory Project/Assets/Scripts/MenuUIHandler.cs
@@ -1,12 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 public class MenuUIHandler : MonoBehaviour
 {
     public TMP_InputField NameField;
+    public String PlayerName;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +23,21 @@ public class MenuUIHandler : MonoBehaviour
     {
         
     }
+    public void NameChanged(){
+        //PlayerNameText = NameField.GetComponent<TextMeshProUGUI>().text;
+        PlayerName = NameField.GetComponent<TMP_InputField>().text;
+        HighscoreTracker.Instance.PlayerName = PlayerName;
+    }
+
     public void OpenMain(){
         SceneManager.LoadScene(1);
+    }
+    public void Exit(){
+        //MainManager.Instance.SaveColor();
+        #if UNITY_EDITOR
+                EditorApplication.ExitPlaymode();
+        #else
+                Application.Quit(); // original code to quit Unity player
+        #endif
     }
 }

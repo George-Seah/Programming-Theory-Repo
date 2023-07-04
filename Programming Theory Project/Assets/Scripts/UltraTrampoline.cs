fb383e4ddd0daccea23a5d1f888a1f6fd3498f56
George-Seah <georgerseah@gmail.com>
2023-07-05 01:13:57 +0800

    Added 2 balls and 2 cubes alongside 2 trampolines and a player avatar model
    Just need to add a Highscore tracker and add functionality to the player.

---
+++ Programming Theory Project/Assets/Scripts/UltraTrampoline.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltraTrampoline : Trampoline
+{
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        bounciness = 1f;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
